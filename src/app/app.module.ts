84419a9ae332587544c9870afdefdd18806e5e62
roishach <roishachar@gmail.com>
2017-03-14 17:14:24 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from "angularfire2";
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAw1_A4ydVzGjpNGkJjsfLsmVGktmZTk2E',
+  authDomain: 'robo-40587.firebaseapp.com',
+  databaseURL: 'https://robo-40587.firebaseio.com',
+  storageBucket: 'robo-40587.appspot.com',
+  messagingSenderId: '422055400564'
+};
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
