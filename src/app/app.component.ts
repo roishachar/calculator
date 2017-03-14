0253a5ac32e13c35c34c891f30e86d5deb169350
roishach <roishachar@gmail.com>
2017-03-14 19:13:16 +0200

    + operator is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,35 +17,42 @@ export class AppComponent {
   constructor(af: AngularFire) {
     this.itemValues = af.database.list('/items', {preserveSnapshot: true});
     this.items = af.database.list('/items');
+    this.resultNumber=0;
 
     this.itemValues.subscribe(snapshots => {
       this.result = '';
       this.resultNumber=0;
-      let lastTypeNumber=false;
+      let lastTypeNumber=true;
       let lastNumber='';
+
 
       snapshots.forEach(snapshot => {
         // console.log(snapshot.val())
 
-        this.result = this.result + snapshot.val()
+        // this.result = this.result + snapshot.val()
         // this.resultNumber =  this.resultNumber + Number(snapshot.val());
 
+        debugger;
         var current = Number(snapshot.val());
 
-        console.log(current);
+        // console.log(current);
         if (!isNaN(current) && lastTypeNumber==false) {
           lastTypeNumber=true;
           this.resultNumber=this.resultNumber+current;
         }
-        if (!isNaN(current) && lastTypeNumber==true) {
+        else if (!isNaN(current) && lastTypeNumber==true) {
           lastTypeNumber=true;
           lastNumber=lastNumber+current;
         }
-        if (!isNaN(current) && lastTypeNumber==true) {
-          lastTypeNumber=true;
-          lastNumber=lastNumber+current;
+        else if (isNaN(current)) {
+          lastTypeNumber=false;
+          this.resultNumber= this.resultNumber+Number(lastNumber);
+          lastNumber='';
         }
-
+        // console.log("last number:");
+        // console.log(lastNumber);
+        // console.log("last type:");
+        // console.log(lastTypeNumber);
 
 
         this.resList.push(Number(snapshot.val()));
@@ -62,12 +69,15 @@ export class AppComponent {
     this.items.remove().then(_ => console.log('deleted!'));
   }
 
-  showResult() {
+  showResult(str) {
     // let res = 0 ;
     // let numbers = [1, 2, 3];
     // for (let num of numbers) {
     //   res=res+num;
     // }
+    var res = this.resultNumber;
+    this.items.remove().then(_ => console.log('deleted!'));
+    this.items.push(res);
     console.log(this.resultNumber);
 
     // Number('1234')
